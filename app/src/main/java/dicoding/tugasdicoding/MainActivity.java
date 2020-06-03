fff2199dd07fc51b93806dc5b3c23f59056059ce
aziyan99 <rajaazian08@gmail.com>
2020-06-03 16:00:44 +0700

    edit profile activity

--- app/src/main/java/dicoding/tugasdicoding/MainActivity.java
+++ app/src/main/java/dicoding/tugasdicoding/MainActivity.java
@@ -28,35 +28,89 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("");
+        }
+
 
         rvKemenlu = findViewById(R.id.rv_data);
 
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL, false);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         rvKemenlu.setLayoutManager(layoutManager);
         rvKemenlu.setHasFixedSize(true);
         getKemenluData();
     }
 
-    void getKemenluData(){
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        KemenluService service = retrofit.create(KemenluService.class);
-        Call<KemenluResult> getKemenlu = service.getKemenlu();
-        getKemenlu.enqueue(new Callback<KemenluResult>() {
-            @Override
-            public void onResponse(Call<KemenluResult> call, Response<KemenluResult> response) {
-                KemenluAdapter adapter = new KemenluAdapter(MainActivity.this, response.body().getResult());
-                adapter.notifyDataSetChanged();
-                rvKemenlu.setAdapter(adapter);
-            }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        boolean isConnected = networkInfo != null &&
+                networkInfo.isConnectedOrConnecting();
+        if(!isConnected){
+            Toast toast = Toast.makeText(getApplicationContext(), "No internet connection", Toast.LENGTH_SHORT);
+            toast.show();
+        }else {
+            rvKemenlu = findViewById(R.id.rv_data);
 
-            @Override
-            public void onFailure(Call<KemenluResult> call, Throwable t) {
-                t.printStackTrace();
-            }
-        });
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+            rvKemenlu.setLayoutManager(layoutManager);
+            rvKemenlu.setHasFixedSize(true);
+            getKemenluData();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.profile, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (item.getItemId() == R.id.goToProfilePage) {
+            Intent profileIntent = new Intent(MainActivity.this, ProfileActivity.class);
+            startActivity(profileIntent);
+            return true;
+        }
+        return false;
+    }
+
+    void getKemenluData(){
+
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        boolean isConnected = networkInfo != null &&
+                networkInfo.isConnectedOrConnecting();
+        if(!isConnected){
+            Toast toast = Toast.makeText(getApplicationContext(), "No internet connection", Toast.LENGTH_LONG);
+            toast.show();
+        }else {
+
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            KemenluService service = retrofit.create(KemenluService.class);
+            Call<KemenluResult> getKemenlu = service.getKemenlu();
+            getKemenlu.enqueue(new Callback<KemenluResult>() {
+                @Override
+                public void onResponse(Call<KemenluResult> call, Response<KemenluResult> response) {
+                    KemenluAdapter adapter = new KemenluAdapter(MainActivity.this, response.body().getResult());
+                    adapter.notifyDataSetChanged();
+                    rvKemenlu.setAdapter(adapter);
+                }
+
+                @Override
+                public void onFailure(Call<KemenluResult> call, Throwable t) {
+                    t.printStackTrace();
+                }
+            });
+        }
     }
 
 }
--- app/src/main/java/dicoding/tugasdicoding/ProfileActivity.java
+++ app/src/main/java/dicoding/tugasdicoding/ProfileActivity.java
@@ -1,14 +1,58 @@
 package dicoding.tugasdicoding;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
-public class Profile extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
+    Button githubBtn;
+    Button showInfoBtn;
+    private static final String textInfo = "Aplikasi Data Duta Luar Negeri Indonesia.\nInformasi lengkap bisa kunjungi link projek di github.\nTerima kasih :)";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setTitle("");
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        githubBtn = findViewById(R.id.btn_github);
+        githubBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/aziyan99/tugas-dicoding"));
+            startActivity(intent);
+        });
+
+        showInfoBtn = findViewById(R.id.btn_info);
+        showInfoBtn.setOnClickListener(v -> showInfo());
+    }
+
+    private void showInfo(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("Info");
+
+        alertDialogBuilder
+                .setMessage(textInfo)
+                .setCancelable(true);
+        AlertDialog alertDialog = alertDialogBuilder.create();
+
+        alertDialog.show();
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
